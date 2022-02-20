dd1b69df57a0f4e847e5555e05660fe2a52484c5
cjobee <calebj@liatrio.com>
2022-02-20 16:55:45 -0600

    feat: updated manifest and timer

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ink-bot
+# InkBot PS plugin
+
+The InkBot Photoshop plugin is designed to improve daily art workflows by doing the following:
+
+- Gathering and providing reference
+- Providing structure in the form of time and prompt
+- Catering to the iterative process by initializing references in layers of various abstraction.
+- Providing automation for the output of completed portraits by posting to social media.
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+function showLayerNames() {
+    const app = window.require("photoshop").app;
+    const allLayers = app.activeDocument.layers;
+    const allLayerNames = allLayers.map(layer => layer.name);
+    const sortedNames = allLayerNames.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
+    document.getElementById("layers").innerHTML = `
+      <ul>${
+        sortedNames.map(name => `<li>${name}</li>`).join("")
+      }</ul>`;
+}
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Ink Bot!\n');
-});
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "Time is up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown( "ten-countdown", 10, 0 );
+
+
+document.getElementById("btnPopulate").addEventListener("click", showLayerNames);
